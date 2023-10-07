f88136de472f914abe33b180bc0fddca854703d4
Adam <adamz1977@hotmail.com>
2023-10-07 12:19:21 -0400

    Update emailwiz_add-user.sh

--- emailwiz_add-user.sh
+++ emailwiz_add-user.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-echo "Enter username:"
+echo "Enter username"
 read username
 
 useradd -m -G mail $username
 
-echo "Enter password:"
+echo "Enter password"
 passwd $username
 
-echo "Enter email address:"
+echo "Enter email address"
 read email
 
 echo "$email $username" >> /etc/postfix/virtual
 
 postmap /etc/postfix/virtual
 systemctl restart postfix
+echo "You can now log into an email program with that username"
