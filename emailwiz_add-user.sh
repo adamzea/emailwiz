8745f5b76f4aa2dcc4121acda08e097b44115cbc
Adam <adamz1977@hotmail.com>
2023-10-07 11:50:40 -0400

    Update emailwiz_add-user.sh

--- emailwiz_add-user.sh
+++ emailwiz_add-user.sh
@@ -11,8 +11,7 @@ passwd $username
 echo "Enter email address:"
 read email
 
-echo "$email $username" >> /etc/postfix/virtual
-/usr/sbin/postmap /etc/postfix/virtual
+"$email $username" >> /etc/postfix/virtual
 
-# postmap /etc/postfix/virtual
+postmap /etc/postfix/virtual
 systemctl restart postfix
